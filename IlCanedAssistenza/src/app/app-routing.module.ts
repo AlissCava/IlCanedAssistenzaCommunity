939417f5bad4aadce6ecd79cd9f3809704f31281
AlissCava <cavalleraalissa@gmail.com>
2025-05-31 12:13:07 +0200

    aggiunta component home e cani

--- IlCanedAssistenza/src/app/app-routing.module.ts
+++ IlCanedAssistenza/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
+// src/app/app-routing.module.ts
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component'; // Importa il tuo HomeComponent
+import { CaniComponent } from './cani/cani.component'; // Importa il tuo CaniComponent
 
-const routes: Routes = [];
+const routes: Routes = [
+  // Questa rotta dice: se l'URL è VUOTO (la radice dell'app), carica HomeComponent
+  { path: '', component: HomeComponent },
+
+  // Questa rotta dice: se l'URL è '/cani', carica CaniComponent
+  { path: 'cani', component: CaniComponent },
+
+  // Puoi aggiungere altre rotte qui per le future pagine, es:
+  // { path: 'gatti', component: GattiComponent },
+
+  // Rotta wildcard (opzionale ma consigliata): gestisce URL non riconosciuti
+  // { path: '**', redirectTo: '' } // Reindirizza a Home se l'URL non corrisponde
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- IlCanedAssistenza/src/app/app.module.ts
+++ IlCanedAssistenza/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CaniComponent } from './cani/cani.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CaniComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
